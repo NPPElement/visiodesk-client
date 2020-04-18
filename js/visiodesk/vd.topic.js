772654472ab8bdb2479e791455cd7da530ee999d
kanarisru <79787246978@yandex.ru>
2020-04-18 21:34:07 +0300

    Сортировка items в topic-ах, добавление групп раньше, чем удаление.

--- js/visiodesk/vd.topic.js
+++ js/visiodesk/vd.topic.js
@@ -981,8 +981,8 @@ window.VD_Topic = (function () {
         }
         itemsForSend.push(fullItemObject);
         itemsForSend.sort((a,b)=>{
-            if(a.type.id===VD_SETTINGS.ITEM_TYPE_ID.user && b.type.id===VD_SETTINGS.ITEM_TYPE_ID.removed_from_user) return -1;
-            if(a.type.id===VD_SETTINGS.ITEM_TYPE_ID.removed_from_user && b.type.id===VD_SETTINGS.ITEM_TYPE_ID.user) return 1;
+            if(a.type.id===VD_SETTINGS.ITEM_TYPE_ID.group && b.type.id===VD_SETTINGS.ITEM_TYPE_ID.removed_from_group) return -1;
+            if(a.type.id===VD_SETTINGS.ITEM_TYPE_ID.removed_from_group && b.type.id===VD_SETTINGS.ITEM_TYPE_ID.group) return 1;
             return 0;
         });
         return fullItemObject;
