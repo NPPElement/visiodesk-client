562d2f7d4d2d5a0c7ec57fc4cfdeb6f8196dbed2
Andrey <git57043>
2021-06-13 11:20:51 +0300

    Управление RPC в настройках REPLACE_RPC_URL_OR_MQTT

--- js/js.settings.js
+++ js/js.settings.js
@@ -1,5 +1,7 @@
 // Указать или не указывать, если обновлять через RPC
-window.REPLACE_RPC_URL_OR_MQTT = "ws://user:user@visiodesk.net:15675/ws/Set";
+// window.REPLACE_RPC_URL_OR_MQTT = "ws://user:user@visiodesk.net:15675/ws/Set";
+
+
 window.WORKSPACE = {
     split: true,
     mqtt: {
